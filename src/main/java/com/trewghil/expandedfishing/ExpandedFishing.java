eaa2ed8bbe148152c7975f2ece443762e1863610
Max Gordon <maxgordonatx@gmail.com>
2020-07-11 15:29:04 -0500

    project setup

---
+++ src/main/java/com/trewghil/expandedfishing/ExpandedFishing.java
@@ -0,0 +1,10 @@
+package com.trewghil.expandedfishing;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExpandedFishing implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
