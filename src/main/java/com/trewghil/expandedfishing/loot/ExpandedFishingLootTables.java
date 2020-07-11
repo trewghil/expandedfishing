6f5672278ff69a5fea58ffaf5225958be9f794f0
Max Gordon <maxgordonatx@gmail.com>
2020-07-11 18:29:16 -0500

    implemented functional night fishing loot table

---
+++ src/main/java/com/trewghil/expandedfishing/loot/ExpandedFishingLootTables.java
@@ -0,0 +1,10 @@
+package com.trewghil.expandedfishing.loot;
+
+import com.trewghil.expandedfishing.ExpandedFishing;
+import net.minecraft.util.Identifier;
+
+public class ExpandedFishingLootTables {
+
+    public static final Identifier NIGHT_FISHING = ExpandedFishing.identify("nightfishing");
+
+}
--- src/main/java/com/trewghil/expandedfishing/mixin/MixinFishingBobberEntity.java
+++ src/main/java/com/trewghil/expandedfishing/mixin/MixinFishingBobberEntity.java
@@ -1,24 +1,29 @@
 package com.trewghil.expandedfishing.mixin;
 
+import com.trewghil.expandedfishing.loot.ExpandedFishingLootTables;
 import net.minecraft.entity.projectile.FishingBobberEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
 
 @Mixin(FishingBobberEntity.class)
 public class MixinFishingBobberEntity {
 
-    @Inject(method = "use", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/loot/LootTable;generateLoot(Lnet/minecraft/loot/context/LootContext;)Ljava/util/List;"))
-    private void nightTimeFish(ItemStack usedItem, CallbackInfoReturnable<Integer> cir) {
-        System.out.println("Loot table generated at ");
+    @ModifyVariable(method = "use(Lnet/minecraft/item/ItemStack;)I", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/loot/LootManager;getTable(Lnet/minecraft/util/Identifier;)Lnet/minecraft/loot/LootTable;"))
+    private LootTable nightTimeFish(LootTable lootTable) {
 
-        if(!((FishingBobberEntity) (Object) this).world.isDay()) {
-            System.out.print("NIGHT");
+        FishingBobberEntity ent = ((FishingBobberEntity) (Object) this);
+
+        if(!ent.world.isDay()) {
+            lootTable = ent.world.getServer().getLootManager().getTable(ExpandedFishingLootTables.NIGHT_FISHING);
         } else {
-            System.out.print("DAY");
+            lootTable = ent.world.getServer().getLootManager().getTable(LootTables.FISHING_GAMEPLAY);
         }
+
+        return lootTable;
     }
 
 }
