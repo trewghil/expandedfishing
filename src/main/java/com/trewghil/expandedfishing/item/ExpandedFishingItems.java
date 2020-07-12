5d8f6e2aff671e564fe851997c39526dc812f386
Max Gordon <maxgordonatx@gmail.com>
2020-07-12 13:50:42 -0500

    added moray eel to nightfishing loot

--- src/main/java/com/trewghil/expandedfishing/item/ExpandedFishingItems.java
+++ src/main/java/com/trewghil/expandedfishing/item/ExpandedFishingItems.java
@@ -8,6 +8,7 @@ import net.minecraft.util.registry.Registry;
 public class ExpandedFishingItems {
 
     public static final Item SNAPPER = new Item(new Item.Settings().group(ItemGroup.FOOD));
+    public static final Item MORAY_EEL = new Item(new Item.Settings().group(ItemGroup.FOOD));
 
     public ExpandedFishingItems() {
 
@@ -15,6 +16,7 @@ public class ExpandedFishingItems {
 
     public static void init() {
         Registry.register(Registry.ITEM, ExpandedFishing.identify("snapper"), SNAPPER);
+        Registry.register(Registry.ITEM, ExpandedFishing.identify("moray_eel"), MORAY_EEL);
     }
 
 }
