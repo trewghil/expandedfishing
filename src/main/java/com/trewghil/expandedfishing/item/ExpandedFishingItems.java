0a0c2f501512ac58e9337a1ffa11166b5c4710c6
AbsolutelyMax <mdaft124@gmail.com>
2020-07-13 18:24:57 -0500

    added cooked snapper and corresponding recipes
    note, night fishing should yield slightly more XP than day fishing. ie 0.4 instead of 0.35 for snapper

--- src/main/java/com/trewghil/expandedfishing/item/ExpandedFishingItems.java
+++ src/main/java/com/trewghil/expandedfishing/item/ExpandedFishingItems.java
@@ -1,6 +1,7 @@
 package com.trewghil.expandedfishing.item;
 
 import com.trewghil.expandedfishing.ExpandedFishing;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
@@ -8,14 +9,14 @@ import net.minecraft.util.registry.Registry;
 public class ExpandedFishingItems {
 
     public static final Item SNAPPER = new Item(new Item.Settings().group(ItemGroup.FOOD));
+    public static final Item COOKED_SNAPPER = new Item(new Item.Settings().group(ItemGroup.FOOD).food(FoodComponents.COOKED_SALMON));
     public static final Item MORAY_EEL = new Item(new Item.Settings().group(ItemGroup.FOOD));
 
-    public ExpandedFishingItems() {
-
-    }
+    public ExpandedFishingItems() {}
 
     public static void init() {
         Registry.register(Registry.ITEM, ExpandedFishing.identify("snapper"), SNAPPER);
+        Registry.register(Registry.ITEM, ExpandedFishing.identify("cooked_snapper"), COOKED_SNAPPER);
         Registry.register(Registry.ITEM, ExpandedFishing.identify("moray_eel"), MORAY_EEL);
     }
 
